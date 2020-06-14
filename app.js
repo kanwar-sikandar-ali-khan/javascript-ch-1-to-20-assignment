7566bd04263a59073863137a8dc96fc0c905c160
kanwar-sikandar-ali-khan <64998093+kanwar-sikandar-ali-khan@users.noreply.github.com>
2020-06-14 05:59:03 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,223 @@
+// CHAPTER 1
+
+// task 1
+
+alert("THANKS FOR VISITING OUR WEBSITE");
+
+// task 2
+
+alert("Error! Please enter a valid password.");
+
+// task 3
+
+alert("Welcome to js Land\nHappy Coding");
+
+// task 4
+
+alert("Welcome to js Land");
+alert("Happy Coding!\nPrevent this page from creating additional dialogs");
+
+// task 5
+
+console.log("Hello.... I can run JS through my web browser's console");
+
+
+
+
+
+// CHAPTER 2
+
+
+// task1
+
+var username = "KANWAR";
+
+// task2
+
+var myName = "KANWAR SIKANDAR ALI KHAN";
+
+// task3
+
+var message = "Hello World";
+
+alert(message);
+
+// task4
+
+
+var name = "saad";
+var age = 15;
+var certification = "certified kubernetes application developer";
+
+alert(name);
+alert(age);
+alert(certification);
+
+// task 5
+
+var food = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(food);
+
+// task6
+
+var email = "kskh1361@gmail.com";
+alert("my email address is "+email);
+
+// task 7
+
+var book = "A smarter way to learn javacript";
+alert("I AM TRYING TO LEARN FROM THE BOOK "+book);
+
+
+// task 8
+
+document.write("yah! i can write html content through javascript");
+
+
+// CHAPTER 3
+
+// task 1
+
+var age = 16;
+
+alert(age);
+
+// task 2
+
+var visit;
+visit = 6;
+
+alert("You have visited this site "+ visit+ " times");
+
+
+// task 3
+
+var birthYear = 2002;
+
+document.write(" </br> My birth year is "+birthYear+ "</br> Date type of Declared Variable is number");
+
+// task 4
+
+var visitor = "Kanwar";
+
+var productTitile = "T-shirt";
+
+var quantitiy = 3;
+
+document.write("</br>"+ visitor+" orderd " + quantitiy +" "+ productTitile + " on imperial clothing store </br>");
+
+
+// chapter4
+
+// task1
+
+var a = ["a","b","c"];
+
+// task2
+
+// legal
+
+var a = "kanwar";
+
+var b = 3;
+
+var newVariable = "legal";
+
+var $another = "legal";
+
+var _haifun = "legal";
+
+// illegal
+
+/*var 1new = "ilegal";
+
+var new variable = "ilegal";
+
+var var = "ilegal";
+
+var let = "ilegal";
+
+var 2another = "ilegal";*/
+
+
+// task 3
+
+
+document.write("</br> RULES FOR NAMING JAVASCRIPT VARIABLES </br> </br> </br> A number can only contain  number , $ , _</br>variables name are case sensitive </br> variable name should not be javascript keyword </br></br> ")
+
+
+
+// chapter5
+
+// task 1
+var a = 2;
+
+var b = 3;
+
+var c = a+b;
+
+document.write("</br>"+"Sum of "+a+" and "+b+" "+ "is " +c);
+
+// task2
+
+var a = 2;
+
+var b = 3;
+
+var c = a-b;
+
+document.write("</br>"+"subtraction of "+a+" and "+b+" "+ "is " +c);
+
+
+var a = 2;
+
+var b = 3;
+
+var c = a*b;
+
+document.write("</br>"+"multiplication of "+a+" and "+b+" "+ "is " +c);
+
+
+
+var a = 2;
+
+var b = 3;
+
+var c = a/b;
+
+document.write("</br>"+"division of "+a+" and "+b+" "+ "is " +c);
+
+
+
+
+var a = 2;
+
+var b = 3;
+
+var c = a%b;
+
+document.write("</br>"+"modulus of "+a+" and "+b+" "+ "is " +c+"</br></br>");
+
+
+
+// task 3
+
+var z;
+
+document.write("value after declaration is "+z+"</br>");
+
+var b = 2 ;
+
+document.write("intial value "+b+"</br>");
+
+document.write("value after increment is "+ ++b +"</br>");
+
+var sum = b+7;
+
+document.write("value after addition of 7 is "+ sum +"</br>");
+
+document.write("value after decrement is "+ --sum +"</br>");
+
+document.write("value after remainder is "+ sum%3 +"</br>");
+
+
